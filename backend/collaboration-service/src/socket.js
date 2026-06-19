f7fffa468cfaf6d8f23b94787849ed3680e6e0d2
koppuhemanthsaikumar <koppuhemanthsaikumar123@gmail.com>
2026-06-19 12:04:02 +0530

    Online members list showing updated

--- backend/collaboration-service/src/socket.js
+++ backend/collaboration-service/src/socket.js
@@ -1,16 +1,37 @@
 function initializeSocket(io) {
   const roomCodeMap = new Map();
+  const roomUsers = new Map();
   io.on("connection", (socket) => {
 
     console.log(`User connected ${socket.id}`);
 
-    socket.on("join-room", (roomId) => {
+    socket.on("join-room",({roomId,userId,username}) => {
+    socket.join(roomId);
 
-        socket.join(roomId);
-        const existingCode = roomCodeMap.get(roomId) || "";
-        socket.emit("initial-code",existingCode);
-      }
-    );
+    const existingCode =roomCodeMap.get(roomId) || "";
+
+    socket.emit("initial-code",existingCode);
+
+    if (!roomUsers.has(roomId)) {
+      roomUsers.set(roomId, []);
+    }
+
+    const users = roomUsers.get(roomId);
+
+    const alreadyExists =users.some(user =>
+          user.socketId === socket.id
+      );
+
+    if (!alreadyExists) {
+      users.push({
+        socketId: socket.id,
+        userId,
+        username
+      });
+    }
+
+      io.to(roomId).emit("participants-update",users);
+    });
 
     socket.on("code-change",({ roomId, code }) => {
 
@@ -22,7 +43,17 @@ function initializeSocket(io) {
 
     socket.on("disconnect",() => {
 
-        console.log(`${socket.id} disconnected`);
+        for (const [roomId, users]of roomUsers.entries()) {
+          const updatedUsers = users.filter(user =>user.socketId !==socket.id);
+
+          roomUsers.set(
+            roomId,updatedUsers
+          );
+
+          io.to(roomId).emit(
+            "participants-update",updatedUsers
+          );
+        }
       }
     );
   });
